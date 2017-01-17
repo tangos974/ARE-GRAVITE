9c5e7ef83c4a27272f77b127c147c35fba66e99d
tangos974 <fremonttanguy@yahoo.fr>
2017-01-17 21:58:56 +0400

    Delete lib_tableaux.py

--- lib_tableaux.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def inversion_vecteur(tableau):
-"""Prend en entrée un vecteur (tableau de dimension 1) et renvoie le même inversé"""
-  for k in range(len(tableau)//2):
-    temp = tableau[k]
-    tableau[k] = tableau[-k-1]
-    tableau[-k-1] = temp
-  return tableau
